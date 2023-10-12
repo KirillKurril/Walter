83f7c0f647ae26ffb6f402c17e2d085953e77cdd
KirillKurril <kirillkurill1525@gmail.com>
2023-10-12 23:21:21 +0300

    четвертая лаба сделана

---
+++ WLWD4/WLWD4/Entities/Computer.cs
@@ -0,0 +1,17 @@
+﻿using WLWD4.Interfaces;
+
+namespace WLWD4.Entities
+{
+    internal class Computer
+    {
+        public int Id { set; get; } 
+        public bool IsAdmin { get; set; }
+        public string Name { get; set; }
+
+        public Computer(int id = -1, bool isAdmin = false, string name = "") 
+            => (Id, IsAdmin, Name) = (id, isAdmin, name);
+
+        public override string ToString()
+            => $"Id: {Id}; IsAdmin: {IsAdmin}; Name: {Name}";
+    }
+}
---
+++ WLWD4/WLWD4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.IO.Enumeration;
+using WLWD4.Entities;
+
+namespace WLWD4
+{
+    internal class Program
+    {
+        static void DataPrint(Dictionary<string, List<Computer>> fileData)
+        {
+            foreach (var file in fileData)
+            {
+                Console.WriteLine($"\nFile name: {file.Key}\n\nFile data:");
+                foreach (var dataList in file.Value)
+                    Console.WriteLine(dataList.ToString());
+            }
+        }
+        static void Main(string[] args)
+        {
+            string mainDir = @"C:\Walter\WLWD4\WLWD4";
+            string curDir = Path.Combine(mainDir, "dmitruk_Lab4");
+            FileService fileHandler = new FileService(curDir);
+
+
+            fileHandler.DirectoryInit();
+            Dictionary<string, string> fileInfo = fileHandler.GetFilesInfo();
+            Console.WriteLine("\nList of files in the current directory:\n\n");
+            foreach (var file in fileInfo)
+                Console.WriteLine($"File name: {file.Key}, file extention: {file.Value}");
+
+
+            fileHandler.FillFiles();
+            Console.WriteLine("\n\nFiles are filled\n\n");
+            Dictionary<string, List<Computer>> fileData = fileHandler.GetFilesData();
+            DataPrint(fileData);
+
+           
+
+            Console.WriteLine("\n\nFile data is sorted\n\n");
+            fileHandler.Sort(fileData);
+            DataPrint(fileData);
+
+        }
+        
+    }
+    
+}
