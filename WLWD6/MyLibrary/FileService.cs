cd3ca329430bf4563dae12404895b2989749ab4d
KirillKurril <kirillkurill1525@gmail.com>
2023-10-31 00:55:33 +0300

    начата шетсая

---
+++ WLWD6/MyLibrary/FileService.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Text.Json;
+using WLWD6.Interfaces;
+
+namespace MyLibrary
+{
+    internal class FileService<T> : IFileService<T>
+    {
+        IEnumerable<T> IFileService<T>.ReadFile(string fileName)
+        {
+            string json = File.ReadAllText(fileName);
+            List<T> list = JsonSerializer.Deserialize<List<T>>(json);
+            return list ?? new List<T>();
+        }
+
+        void IFileService<T>.SaveData(IEnumerable<T> data, string fileName)
+        {
+            string json = JsonSerializer.Serialize(data);
+            File.WriteAllText(fileName, json);
+        }
+    }
+}
