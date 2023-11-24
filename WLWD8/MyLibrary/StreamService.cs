ea5c972efab5370578bb0b0a4eebec8ccd6ba003
KirillKurril <kirillkurill1525@gmail.com>
2023-11-24 21:11:45 +0300

    8+

---
+++ WLWD8/MyLibrary/StreamService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class StreamService<T>
+    {
+        public async Task WriteToStreamAsync(Stream stream, IEnumerable<T> data/*, IProgress<string> progress*/)
+        {
+
+            await Task.Run(() =>
+            {
+                Thread.Sleep(100);
+                Console.WriteLine("МЕНЯ ЕБАЛИ ТУ ТУН ТУН");
+            });
+            
+        }
+
+        public async Task CopyFromStreamAsync(Stream stream, string filename, IProgress<string> progress)
+        {
+
+        }
+
+        public async Task<int> GetStatisticsAsync(string fileName, Func<T, bool> filter)
+        {
+            return 0;
+        }
+    }
+}
---
+++ WLWD8/WLWD8/Program.cs
@@ -0,0 +1,16 @@
+﻿using MyLibrary;
+
+namespace WLWD8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            StreamService<int> ss = new StreamService<int>();
+            int[] arr = new int[10];
+            ss.WriteToStreamAsync(new MemoryStream(new byte[] { }), arr);
+            Console.WriteLine("А МЕНЯ ЕБАЛИ");
+        }
+    }
+}
